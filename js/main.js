ae252bc82398ae88a3d0e219076f3a0946a9fb07
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2016-02-29 18:29:48 +0700

    Darth edXius commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var indonesiaxModule = angular.module('indonesiaxModule', []);
+// StudioQuiz function 
+function studioquiz($scope, $sce) {
+    $scope.quiz = {
+        quiestion: "quest",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+        answera: "()",
+        answerb: "()",
+        answerc: "()",
+        answerd: "()"
+    };
+}
